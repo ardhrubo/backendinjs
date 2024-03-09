fc130c1964d14ca1b8c538c8e6af55a6ff7fd40b
ardhrubo <ardhrubo3038@gmail.com>
2024-03-09 23:31:51 +0600

    new appp

--- backendtutorial/frontend/src/App.jsx
+++ backendtutorial/frontend/src/App.jsx
@@ -1,14 +1,36 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [jokes,setJokes] = useState([])
+
+  useEffect(()=>{
+    axios.get('/api/jokes')
+    .then((response)=>{
+      setJokes(response.data)
+    })
+    .catch((err)=>{
+      console.log(err)
+    })
+  })
+
+
 
   return (
     <>
       <h1> Dhrubo the Boss</h1>
+      <p> Jokes: {jokes.length}</p>
+      {
+        jokes.map((joke,index)=> (
+           <div key={joke.id}>
+            <h3>{joke.title}</h3>
+            <p>{joke.content}</p>
+           </div>
+        ))
+      }
     </>
   )
 }
